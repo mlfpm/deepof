76cde4ab2778dc3c00b1d39d8e68c0bde5edc296
lucas_miranda <lucasmiranda42@gmail.com>
2020-05-14 17:24:28 +0200

    Cleans hyperparameter_tuning.py

--- source/hyperparameter_tuning.py
+++ source/hyperparameter_tuning.py
@@ -11,10 +11,9 @@ script, input_type, path = argv
 with open(path + "DLC_social_1_exp_conditions.pickle", "rb") as handle:
     Treatment_dict = pickle.load(handle)
 
-DLC_social_1 = Project(
+DLC_social_1 = project(
     path=path,  # Path where to find the required files
-    p=cpu_count(),  # Number of processes used for parallelization
-    smooth_alpha=0.1,  # Alpha value for exponentially weighted smoothing
+    smooth_alpha=0.85,  # Alpha value for exponentially weighted smoothing
     distances=[
         "B_Center",
         "B_Nose",
