7a54765a4d3d83f1f71096df33bd22881bf7896e
lucas_miranda <lucasmiranda42@gmail.com>
2020-05-18 17:10:40 +0200

    Reduced batch size in hyperparameter_tuning.py

--- hyperparameter_tuning.py
+++ hyperparameter_tuning.py
@@ -113,7 +113,7 @@ def tune_search(train, test, project_name):
         epochs=30,
         validation_data=(test, test),
         verbose=1,
-        batch_size=1024,
+        batch_size=256,
         callbacks=[
             tensorboard_callback,
             tf.keras.callbacks.EarlyStopping("val_loss", patience=3),
