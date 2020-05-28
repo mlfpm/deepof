4dca82596f15df0e74c842dff4bd45c738ad48b7
lucas_miranda <lucasmiranda42@gmail.com>
2020-05-28 14:09:49 +0200

    Drafted skeleton of SEQ2SEQ_VAEP (Variational AutoEncoding Predictor) in models.py

--- source/models.py
+++ source/models.py
@@ -315,8 +315,173 @@ class SEQ_2_SEQ_VAE:
         return encoder, generator, vae
 
 
-class SEQ_2_SEQ_VAME:
-    pass
+class SEQ_2_SEQ_VAEP:
+    def __init__(
+            self,
+            input_shape,
+            CONV_filters=256,
+            LSTM_units_1=256,
+            LSTM_units_2=64,
+            DENSE_2=64,
+            DROPOUT_RATE=0.25,
+            ENCODING=32,
+            learn_rate=1e-3,
+            loss="ELBO+MMD",
+    ):
+        self.input_shape = input_shape
+        self.CONV_filters = CONV_filters
+        self.LSTM_units_1 = LSTM_units_1
+        self.LSTM_units_2 = LSTM_units_2
+        self.DENSE_1 = LSTM_units_2
+        self.DENSE_2 = DENSE_2
+        self.DROPOUT_RATE = DROPOUT_RATE
+        self.ENCODING = ENCODING
+        self.learn_rate = learn_rate
+        self.loss = loss
+
+    def build(self):
+        # Encoder Layers
+        Model_E0 = tf.keras.layers.Conv1D(
+            filters=self.CONV_filters,
+            kernel_size=5,
+            strides=1,
+            padding="causal",
+            activation="relu",
+            kernel_initializer=he_uniform(),
+        )
+        Model_E1 = Bidirectional(
+            LSTM(
+                self.LSTM_units_1,
+                activation="tanh",
+                return_sequences=True,
+                kernel_constraint=UnitNorm(axis=0),
+            )
+        )
+        Model_E2 = Bidirectional(
+            LSTM(
+                self.LSTM_units_2,
+                activation="tanh",
+                return_sequences=False,
+                kernel_constraint=UnitNorm(axis=0),
+            )
+        )
+        Model_E3 = Dense(
+            self.DENSE_1,
+            activation="relu",
+            kernel_constraint=UnitNorm(axis=0),
+            kernel_initializer=he_uniform(),
+        )
+        Model_E4 = Dense(
+            self.DENSE_2,
+            activation="relu",
+            kernel_constraint=UnitNorm(axis=0),
+            kernel_initializer=he_uniform(),
+        )
+        Model_E5 = Dense(
+            self.ENCODING,
+            activation="relu",
+            kernel_constraint=UnitNorm(axis=1),
+            activity_regularizer=UncorrelatedFeaturesConstraint(3, weightage=1.0),
+            kernel_initializer=Orthogonal(),
+        )
+
+        # Decoder layers
+
+        Model_D0 = DenseTranspose(
+            Model_E5, activation="relu", output_dim=self.ENCODING,
+        )
+        Model_D1 = DenseTranspose(Model_E4, activation="relu", output_dim=self.DENSE_2, )
+        Model_D2 = DenseTranspose(Model_E3, activation="relu", output_dim=self.DENSE_1, )
+        Model_D3 = RepeatVector(self.input_shape[1])
+        Model_D4 = Bidirectional(
+            LSTM(
+                self.LSTM_units_1,
+                activation="tanh",
+                return_sequences=True,
+                kernel_constraint=UnitNorm(axis=1),
+            )
+        )
+        Model_D5 = Bidirectional(
+            LSTM(
+                self.LSTM_units_1,
+                activation="sigmoid",
+                return_sequences=True,
+                kernel_constraint=UnitNorm(axis=1),
+            )
+        )
+
+        # Define and instanciate encoder
+        x = Input(shape=self.input_shape[1:])
+        encoder = Model_E0(x)
+        encoder = BatchNormalization()(encoder)
+        encoder = Model_E1(encoder)
+        encoder = BatchNormalization()(encoder)
+        encoder = Model_E2(encoder)
+        encoder = BatchNormalization()(encoder)
+        encoder = Model_E3(encoder)
+        encoder = BatchNormalization()(encoder)
+        encoder = Dropout(self.DROPOUT_RATE)(encoder)
+        encoder = Model_E4(encoder)
+        encoder = BatchNormalization()(encoder)
+        encoder = Model_E5(encoder)
+
+        z_mean = Dense(self.ENCODING)(encoder)
+        z_log_sigma = Dense(self.ENCODING)(encoder)
+
+        if "ELBO" in self.loss:
+            z_mean, z_log_sigma = KLDivergenceLayer()([z_mean, z_log_sigma])
+
+        z = Lambda(sampling)([z_mean, z_log_sigma])
+
+        if "MMD" in self.loss:
+            z = MMDiscrepancyLayer()(z)
+
+        # Define and instanciate generator
+        generator = Model_D0(z)
+        generator = BatchNormalization()(generator)
+        generator = Model_D1(generator)
+        generator = BatchNormalization()(generator)
+        generator = Model_D2(generator)
+        generator = BatchNormalization()(generator)
+        generator = Model_D3(generator)
+        generator = BatchNormalization()(generator)
+        generator = Model_D4(generator)
+        generator = BatchNormalization()(generator)
+        generator = Model_D5(generator)
+        x_decoded_mean = TimeDistributed(Dense(self.input_shape[2]))(generator)
+
+        # end-to-end autoencoder
+        encoder = Model(x, z_mean, name="SEQ_2_SEQ_VEncoder")
+        vaep = Model(x, x_decoded_mean, name="SEQ_2_SEQ_VAE")
+
+        # Build generator as a separate entity
+        g = Input(shape=self.ENCODING)
+        _generator = Model_D0(g)
+        _generator = BatchNormalization()(_generator)
+        _generator = Model_D1(_generator)
+        _generator = BatchNormalization()(_generator)
+        _generator = Model_D2(_generator)
+        _generator = BatchNormalization()(_generator)
+        _generator = Model_D3(_generator)
+        _generator = BatchNormalization()(_generator)
+        _generator = Model_D4(_generator)
+        _generator = BatchNormalization()(_generator)
+        _generator = Model_D5(_generator)
+        _x_decoded_mean = TimeDistributed(Dense(self.input_shape[2]))(_generator)
+        generator = Model(g, _x_decoded_mean, name="SEQ_2_SEQ_VGenerator")
+
+        def huber_loss(x_, x_decoded_mean_):
+            huber = Huber(reduction="sum", delta=100.0)
+            return self.input_shape[1:] * huber(x_, x_decoded_mean_)
+
+        vaep.compile(
+            loss=huber_loss,
+            optimizer=Adam(lr=self.learn_rate, ),
+            metrics=["mae"],
+            experimental_run_tf_function=False,
+        )
+
+        return encoder, generator, vaep
 
 
 class SEQ_2_SEQ_MMVAE:
