9e64672706b2c24c6add33d919f2b65f2d2cc05e
lucas_miranda <lucasmiranda42@gmail.com>
2020-07-17 19:26:16 +0200

    Modified LSTMs to work with cuDNN implementation

--- source/models.py
+++ source/models.py
@@ -467,7 +467,6 @@ class SEQ_2_SEQ_GMVAE:
 #       - data augmentation with rotation
 #       - design clustering-conscious hyperparameter tuning pipeline
 #       - execute the pipeline ;)
-#       - compare prior and posterior
 
 # TODO (in the non-immediate future):
 #       - Try Bayesian nets!
