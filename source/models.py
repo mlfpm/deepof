09a87d512a25e9efd42f67ebddc3d9910a98af9d
lucas_miranda <lucasmiranda42@gmail.com>
2020-07-20 22:14:20 +0200

    test posterior with constant variance per component

--- source/models.py
+++ source/models.py
@@ -306,9 +306,9 @@ class SEQ_2_SEQ_GMVAE:
         z_cat = Dense(self.number_of_components, activation="softmax",)(encoder)
         z_cat = Entropy_regulariser(self.entropy_reg_weight)(z_cat)
         z_gauss = Dense(
-            tfpl.IndependentNormal.params_size(
-                self.ENCODING * self.number_of_components
-            ),
+            #tfpl.IndependentNormal.params_size(
+                self.ENCODING * self.number_of_components,
+            #),
             activation=None,
         )(encoder)
 
@@ -326,7 +326,7 @@ class SEQ_2_SEQ_GMVAE:
                     tfd.Independent(
                         tfd.Normal(
                             loc=gauss[1][..., : self.ENCODING, k],
-                            scale=softplus(gauss[1][..., self.ENCODING :, k]),
+                            scale=tf.ones(shape=self.ENCODING)#softplus(gauss[1][..., self.ENCODING :, k]),
                         ),
                         reinterpreted_batch_ndims=1,
                     )
