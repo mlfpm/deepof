88900d600466f3547a29639de0878f7b04c140a6
lucas_miranda <lucasmiranda42@gmail.com>
2020-07-23 20:34:39 +0200

    Implemented Montecarlo Dropout custom layer

--- source/models.py
+++ source/models.py
@@ -346,7 +346,7 @@ class SEQ_2_SEQ_GMVAE:
                     for k in range(self.number_of_components)
                 ],
             ),
-            activity_regularizer=UncorrelatedFeaturesConstraint(3, weightage=1.0),
+            # activity_regularizer=UncorrelatedFeaturesConstraint(3, weightage=1.0),
         )([z_cat, z_gauss])
 
         # Define and control custom loss functions
