70230f95d8b7d428c03b4f88ad11e7437dad7f9a
lucas_miranda <lucasmiranda42@gmail.com>
2020-07-23 17:28:21 +0200

    added Nesterov trick to Adam optimisers

--- source/models.py
+++ source/models.py
@@ -10,7 +10,7 @@ from tensorflow.keras.layers import BatchNormalization, Bidirectional
 from tensorflow.keras.layers import Dense, Dropout, LSTM
 from tensorflow.keras.layers import RepeatVector, Reshape, TimeDistributed
 from tensorflow.keras.losses import Huber
-from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.optimizers import Nadam
 from source.model_utils import *
 import tensorflow as tf
 import tensorflow_probability as tfp
@@ -47,7 +47,7 @@ class SEQ_2_SEQ_AE:
             kernel_size=5,
             strides=1,
             padding="causal",
-            activation="elu",
+            activation="relu",
             kernel_initializer=he_uniform(),
         )
         Model_E1 = Bidirectional(
@@ -70,19 +70,19 @@ class SEQ_2_SEQ_AE:
         )
         Model_E3 = Dense(
             self.DENSE_1,
-            activation="elu",
+            activation="relu",
             kernel_constraint=UnitNorm(axis=0),
             kernel_initializer=he_uniform(),
         )
         Model_E4 = Dense(
             self.DENSE_2,
-            activation="elu",
+            activation="relu",
             kernel_constraint=UnitNorm(axis=0),
             kernel_initializer=he_uniform(),
         )
         Model_E5 = Dense(
             self.ENCODING,
-            activation="elu",
+            activation="relu",
             kernel_constraint=UnitNorm(axis=1),
             activity_regularizer=UncorrelatedFeaturesConstraint(3, weightage=1.0),
             kernel_initializer=Orthogonal(),
@@ -90,10 +90,10 @@ class SEQ_2_SEQ_AE:
 
         # Decoder layers
         Model_D0 = DenseTranspose(
-            Model_E5, activation="elu", output_dim=self.ENCODING,
+            Model_E5, activation="relu", output_dim=self.ENCODING,
         )
-        Model_D1 = DenseTranspose(Model_E4, activation="elu", output_dim=self.DENSE_2,)
-        Model_D2 = DenseTranspose(Model_E3, activation="elu", output_dim=self.DENSE_1,)
+        Model_D1 = DenseTranspose(Model_E4, activation="relu", output_dim=self.DENSE_2,)
+        Model_D2 = DenseTranspose(Model_E3, activation="relu", output_dim=self.DENSE_1,)
         Model_D3 = RepeatVector(self.input_shape[1])
         Model_D4 = Bidirectional(
             LSTM(
@@ -148,7 +148,7 @@ class SEQ_2_SEQ_AE:
 
         model.compile(
             loss=Huber(reduction="sum", delta=100.0),
-            optimizer=Adam(lr=self.learn_rate, clipvalue=0.5,),
+            optimizer=Nadam(lr=self.learn_rate, clipvalue=0.5,),
             metrics=["mae"],
         )
 
@@ -224,7 +224,7 @@ class SEQ_2_SEQ_GMVAE:
             kernel_size=5,
             strides=1,
             padding="causal",
-            activation="elu",
+            activation="relu",
             kernel_initializer=he_uniform(),
         )
         Model_E1 = Bidirectional(
@@ -247,13 +247,13 @@ class SEQ_2_SEQ_GMVAE:
         )
         Model_E3 = Dense(
             self.DENSE_1,
-            activation="elu",
+            activation="relu",
             kernel_constraint=UnitNorm(axis=0),
             kernel_initializer=he_uniform(),
         )
         Model_E4 = Dense(
             self.DENSE_2,
-            activation="elu",
+            activation="relu",
             kernel_constraint=UnitNorm(axis=0),
             kernel_initializer=he_uniform(),
         )
@@ -264,10 +264,10 @@ class SEQ_2_SEQ_GMVAE:
         Model_B3 = BatchNormalization()
         Model_B4 = BatchNormalization()
         Model_D1 = Dense(
-            self.DENSE_2, activation="elu", kernel_initializer=he_uniform()
+            self.DENSE_2, activation="relu", kernel_initializer=he_uniform()
         )
         Model_D2 = Dense(
-            self.DENSE_1, activation="elu", kernel_initializer=he_uniform()
+            self.DENSE_1, activation="relu", kernel_initializer=he_uniform()
         )
         Model_D3 = RepeatVector(self.input_shape[1])
         Model_D4 = Bidirectional(
@@ -387,11 +387,11 @@ class SEQ_2_SEQ_GMVAE:
         if self.predictor > 0:
             # Define and instantiate predictor
             predictor = Dense(
-                self.DENSE_2, activation="elu", kernel_initializer=he_uniform()
+                self.DENSE_2, activation="relu", kernel_initializer=he_uniform()
             )(z)
             predictor = BatchNormalization()(predictor)
             predictor = Dense(
-                self.DENSE_1, activation="elu", kernel_initializer=he_uniform()
+                self.DENSE_1, activation="relu", kernel_initializer=he_uniform()
             )(predictor)
             predictor = BatchNormalization()(predictor)
             predictor = RepeatVector(self.input_shape[1])(predictor)
@@ -452,7 +452,7 @@ class SEQ_2_SEQ_GMVAE:
 
         gmvaep.compile(
             loss=huber_loss,
-            optimizer=Adam(lr=self.learn_rate,),
+            optimizer=Nadam(lr=self.learn_rate,),
             metrics=["mae"],
             loss_weights=([1, self.predictor] if self.predictor > 0 else [1]),
         )
