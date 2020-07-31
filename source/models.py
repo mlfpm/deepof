696eab026e838a37869bf219a608648c03973da1
lucas_miranda <lucasmiranda42@gmail.com>
2020-07-31 21:16:59 +0200

    Updated preprocess method in preprocess.py; now the test/train split works separating independent videos

--- source/models.py
+++ source/models.py
@@ -318,7 +318,6 @@ class SEQ_2_SEQ_GMVAE:
         z_cat = Dense(
             self.number_of_components,
             activation="softmax",
-            activity_regularizer=tf.keras.regularizers.l1(),
         )(encoding_shuffle)
         z_cat = Entropy_regulariser(self.entropy_reg_weight)(z_cat)
         z_gauss = Dense(
