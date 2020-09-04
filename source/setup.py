840e3d9e4507a83d2a957928db4c22fc91f81ee4
lucas_miranda <lucasmiranda42@gmail.com>
2020-09-04 18:39:24 +0200

    Edited test_utils.py

--- source/setup.py
+++ source/setup.py
@@ -1,3 +1,25 @@
-from setuptools import setup, find_packages
+import setuptools
 
-setup(name="deepof", version="0.0.1", packages=find_packages())
+with open("../README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deepof",
+    version="0.0.1",
+    author="Lucas Miranda",
+    author_email="lucas_miranda@psych.mpg.de",
+    description="Unsupervised animal pose classification using Gaussian Mixture Variational Autoencoders",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://gitlab.mpcdf.mpg.de/lucasmir/deepof/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Machine Learning :: Unsupervised Deep Learning",
+        "Google :: Tensorflow",
+        "Neuroscience :: Animal behaviour",
+    ],
+    python_requires=">=3.6",
+)
