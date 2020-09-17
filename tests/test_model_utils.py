e9a057e5aba73ab8cb27fee5f430b094742cc7d6
lucas_miranda <lucasmiranda42@gmail.com>
2020-09-17 12:04:48 +0200

    Initialised testing modules for model_utils.py and visuals.py

---
+++ tests/test_model_utils.py
@@ -0,0 +1,9 @@
+# @author lucasmiranda42
+
+from hypothesis import given
+from hypothesis import settings
+from hypothesis import strategies as st
+from collections import defaultdict
+from deepof.utils import *
+import deepof.preprocess
+import pytest
