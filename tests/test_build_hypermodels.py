6d0f2a5c478cec56d1f07adc223e789a2eb0e17e
lucas_miranda <lucasmiranda42@gmail.com>
2020-09-22 19:30:16 +0200

    Excluded debug regions from coverage report - added code climate template to .gitlab-ci.yml

--- tests/test_build_hypermodels.py
+++ tests/test_build_hypermodels.py
@@ -19,43 +19,43 @@ import tensorflow as tf
 tf.config.experimental_run_functions_eagerly(True)
 
 
-# @settings(deadline=None)
-# @given(
-#     input_shape=st.tuples(
-#         st.integers(min_value=100, max_value=1000),
-#         st.integers(min_value=5, max_value=15),
-#         st.integers(min_value=5, max_value=15),
-#     )
-# )
-# def test_SEQ_2_SEQ_AE_hypermodel_build(input_shape):
-#     deepof.hypermodels.SEQ_2_SEQ_AE(input_shape=input_shape).build(hp=HyperParameters())
-#
-#
-# @settings(deadline=None)
-# @given(
-#     loss=st.one_of(st.just("ELBO"), st.just("MMD"), st.just("ELBO+MMD")),
-#     kl_warmup_epochs=st.integers(min_value=0, max_value=5),
-#     mmd_warmup_epochs=st.integers(min_value=0, max_value=5),
-#     number_of_components=st.integers(min_value=1, max_value=5),
-#     predictor=st.booleans(),
-#     overlap_loss=st.booleans(),
-#     entropy_reg_weight=st.one_of(st.just(0.0), st.just(1.0)),
-# )
-# def test_SEQ_2_SEQ_GMVAE_hypermodel_build(
-#     loss,
-#     kl_warmup_epochs,
-#     mmd_warmup_epochs,
-#     number_of_components,
-#     predictor,
-#     overlap_loss,
-#     entropy_reg_weight,
-# ):
-#     deepof.hypermodels.SEQ_2_SEQ_GMVAE(
-#         input_shape=(100, 15, 10,),
-#         batch_size=10,
-#         loss=loss,
-#         kl_warmup_epochs=kl_warmup_epochs,
-#         mmd_warmup_epochs=mmd_warmup_epochs,
-#         number_of_components=number_of_components,
-#         predictor=predictor,
-#     ).build(hp=HyperParameters())
+@settings(deadline=None)
+@given(
+    input_shape=st.tuples(
+        st.integers(min_value=100, max_value=1000),
+        st.integers(min_value=5, max_value=15),
+        st.integers(min_value=5, max_value=15),
+    )
+)
+def test_SEQ_2_SEQ_AE_hypermodel_build(input_shape):
+    deepof.hypermodels.SEQ_2_SEQ_AE(input_shape=input_shape).build(hp=HyperParameters())
+
+
+@settings(deadline=None)
+@given(
+    loss=st.one_of(st.just("ELBO"), st.just("MMD"), st.just("ELBO+MMD")),
+    kl_warmup_epochs=st.integers(min_value=0, max_value=5),
+    mmd_warmup_epochs=st.integers(min_value=0, max_value=5),
+    number_of_components=st.integers(min_value=1, max_value=5),
+    predictor=st.booleans(),
+    overlap_loss=st.booleans(),
+    entropy_reg_weight=st.one_of(st.just(0.0), st.just(1.0)),
+)
+def test_SEQ_2_SEQ_GMVAE_hypermodel_build(
+    loss,
+    kl_warmup_epochs,
+    mmd_warmup_epochs,
+    number_of_components,
+    predictor,
+    overlap_loss,
+    entropy_reg_weight,
+):
+    deepof.hypermodels.SEQ_2_SEQ_GMVAE(
+        input_shape=(100, 15, 10,),
+        batch_size=10,
+        loss=loss,
+        kl_warmup_epochs=kl_warmup_epochs,
+        mmd_warmup_epochs=mmd_warmup_epochs,
+        number_of_components=number_of_components,
+        predictor=predictor,
+    ).build(hp=HyperParameters())
