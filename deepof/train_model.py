4211a9956c96da624a5be88f1c50d7806e221b87
lucas_miranda <lucasmiranda42@gmail.com>
2020-10-14 12:17:06 +0200

    Fixed bug in data.py

--- deepof/train_model.py
+++ deepof/train_model.py
@@ -247,12 +247,12 @@ coords = project_coords.get_coords(
     align=animal_id + undercond + "Nose",
     align_inplace=True,
 )
-distances = project_coords.get_distances()
-angles = project_coords.get_angles()
-coords_distances = merge_tables(coords, distances)
-coords_angles = merge_tables(coords, angles)
-dists_angles = merge_tables(distances, angles)
-coords_dist_angles = merge_tables(coords, distances, angles)
+#distances = project_coords.get_distances()
+#angles = project_coords.get_angles()
+#coords_distances = merge_tables(coords, distances)
+#coords_angles = merge_tables(coords, angles)
+#dists_angles = merge_tables(distances, angles)
+#coords_dist_angles = merge_tables(coords, distances, angles)
 
 
 def batch_preprocess(tab_dict):
@@ -271,12 +271,12 @@ def batch_preprocess(tab_dict):
 
 input_dict_train = {
     "coords": coords,
-    "dists": distances,
-    "angles": angles,
-    "coords+dist": coords_distances,
-    "coords+angle": coords_angles,
-    "dists+angle": dists_angles,
-    "coords+dist+angle": coords_dist_angles,
+#    "dists": distances,
+#    "angles": angles,
+#    "coords+dist": coords_distances,
+#    "coords+angle": coords_angles,
+#    "dists+angle": dists_angles,
+#    "coords+dist+angle": coords_dist_angles,
 }
 
 print("Preprocessing data...")
