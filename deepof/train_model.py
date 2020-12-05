c8510ba6905e1eacc81f701f33cdc05dc02af919
lucas_miranda <lucasmiranda42@gmail.com>
2020-12-05 02:59:41 +0100

    Added hyperparameter logging for run trial and cluster number

--- deepof/train_model.py
+++ deepof/train_model.py
@@ -475,7 +475,7 @@ if not tune:
             history = gmvaep.fit(
                 x=Xs,
                 y=ys,
-                epochs=3,
+                epochs=250,
                 batch_size=batch_size,
                 verbose=1,
                 validation_data=(
