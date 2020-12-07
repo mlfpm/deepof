11075f367e50d18be866d359f9c0881dd0149a76
lucas_miranda <lucasmiranda42@gmail.com>
2020-12-07 22:17:02 +0100

    Added CustomStopper class to train_utils.py, to start early stopping only after annealing is over

--- deepof/train_model.py
+++ deepof/train_model.py
@@ -59,7 +59,7 @@ parser.add_argument(
     "-exc",
     help="Excludes the indicated bodyparts from all analyses. It should consist of several values separated by commas",
     type=str,
-    default=None,
+    default="",
 )
 parser.add_argument(
     "--gaussian-filter",
@@ -274,7 +274,7 @@ project_coords = project(
     animal_ids=tuple([animal_id]),
     arena="circular",
     arena_dims=tuple([arena_dims]),
-    exclude_bodyparts=exclude_bodyparts,
+    exclude_bodyparts=(None if exclude_bodyparts == "" else exclude_bodyparts),
     exp_conditions=treatment_dict,
     path=train_path,
     smooth_alpha=smooth_alpha,
