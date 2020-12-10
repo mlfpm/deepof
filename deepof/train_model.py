cf490040f3740327d17444d50d3f4f0c9e708476
lucas_miranda <lucasmiranda42@gmail.com>
2020-12-10 13:00:34 +0100

    Added phenotype classification to train_model.py

--- deepof/train_model.py
+++ deepof/train_model.py
@@ -515,7 +515,7 @@ if not tune:
             history = gmvaep.fit(
                 x=Xs,
                 y=ys,
-                epochs=1,
+                epochs=35,
                 batch_size=batch_size,
                 verbose=1,
                 validation_data=(
