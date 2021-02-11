b48ba7d6a9acde91dca9bec017b4a1d1042546bd
lucas_miranda <lucasmiranda42@gmail.com>
2021-02-11 18:05:38 +0100

    Added looking around as a tag for single mice

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -5,10 +5,9 @@ mkdir snakemake_logs
 snakemake --snakefile deepof_experiments.smk --forceall --dag | dot -Tpdf > deepof_experiments_DAG.pdf
 snakemake --snakefile deepof_experiments.smk --forceall --rulegraph | dot -Tpdf > deepof_experiments_RULEGRAPH.pdf
 
-source /mpcdf/soft/distribution/obs_modules.sh
 module load anaconda/3
 module load intel/18.0.5
-module load cuda/10.2
+module load cuda/10.1
 module load cudnn/8.0.4
 module load nccl/2.7.8
 module load tensorrt/7.1.3
