c69fbd29e1050401bb2eb12ae211ba5ed5b5871c
lucas_miranda <lucasmiranda42@gmail.com>
2021-02-11 19:34:49 +0100

    Added looking around as a tag for single mice

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -9,7 +9,7 @@ module load anaconda/3
 module load intel/18.0.5
 module load cuda/10.1
 module load cudnn/7.6.5
-module load nccl/2.7.8
-module load tensorrt/7.1.3
+module load nccl/2.6.4
+module load tensorrt/6.0.1
 
 pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 15 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
