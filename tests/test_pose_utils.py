3070885a63e96cd09f39445b37c58aa01d0cd5f7
lucas_miranda <lucasmiranda42@gmail.com>
2021-02-11 11:02:00 +0100

    Added digging to the list of detected behaviours by deepof.data.coordinates.rule_based_annotation()

--- tests/test_pose_utils.py
+++ tests/test_pose_utils.py
@@ -377,7 +377,7 @@ def test_rule_based_tagging(multi_animal, video_output):
     )
 
     assert type(hardcoded_tags) == deepof.data.table_dict
-    assert list(hardcoded_tags.values())[0].shape[1] == (13 if multi_animal else 3)
+    assert list(hardcoded_tags.values())[0].shape[1] == (15 if multi_animal else 4)
 
 
 # TODO:
