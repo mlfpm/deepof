5e4dbf7b4bc0b21f5bfb3500da34e554b3c09803
lucas_miranda <lucasmiranda42@gmail.com>
2021-03-29 13:43:33 +0200

    Updated .ipynb files to follow Black style guidelines

--- deepof/model_utils.py
+++ deepof/model_utils.py
@@ -287,9 +287,7 @@ class neighbor_latent_entropy(tf.keras.callbacks.Callback):
             with writer.as_default():
                 tf.summary.scalar(
                     "average_neighborhood_cluster_entropy",
-                    data=np.average(
-                        purity_vector,
-                        weights=max_groups[random_idxs]),
+                    data=np.average(purity_vector, weights=max_groups[random_idxs]),
                     step=epoch,
                 )
                 tf.summary.scalar(
