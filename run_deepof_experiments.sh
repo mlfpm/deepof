c923ea47e389347ff1e1c9fda86bfcde5ac34c49
lucas_miranda <lucasmiranda42@gmail.com>
2021-04-19 16:38:45 +0200

    Added extra branch to main autoencoder for rule_based prediction

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -12,4 +12,4 @@ module load cudnn/7.6.5
 module load nccl/2.6.4
 module load tensorrt/6.0.1
 
-pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 15 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
+pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 20 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
