6e8ecc81bc4d9873e1ca1e4283cda1e429305c2f
lucas_miranda <lucasmiranda42@gmail.com>
2021-04-26 18:52:16 +0200

    Added a MirroredStrategy to train models on multiple GPUs if they are available

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -6,10 +6,7 @@ snakemake --snakefile deepof_experiments.smk --forceall --dag | dot -Tpdf > deep
 snakemake --snakefile deepof_experiments.smk --forceall --rulegraph | dot -Tpdf > deepof_experiments_RULEGRAPH.pdf
 
 module load anaconda/3
-module load intel/18.0.5
-module load cuda/10.1
-module load cudnn/7.6.5
-module load nccl/2.6.4
-module load tensorrt/6.0.1
+module load cuda/10.2
+module load tensorflow/gpu/2.4.0
 
 pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 20 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
