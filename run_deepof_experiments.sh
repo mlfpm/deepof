ed105bf6daadbbf99c985c7159d935202b0008b0
Lucas Miranda <lucas_miranda@psych.mpg.de>
2021-11-18 11:08:22 +0100

    updated cluster files

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -5,9 +5,4 @@ mkdir snakemake_logs
 snakemake --snakefile deepof_experiments.smk --forceall --dag | dot -Tpdf > deepof_experiments_DAG.pdf
 snakemake --snakefile deepof_experiments.smk --forceall --rulegraph | dot -Tpdf > deepof_experiments_RULEGRAPH.pdf
 
-module load anaconda/3
-module load tensorflow/gpu/2.4.0
-module load cuda/10.2
-module load cudnn/8.0.4
-
 pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 20 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:2" > deepof_experiments.out 2> deepof_experiments.err
