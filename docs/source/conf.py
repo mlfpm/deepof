11a4c015cd9114af1bac9029b3159b294a227ce1
Lucas Miranda <lucasmiranda42@gmail.com>
2021-12-29 02:01:44 +0100

    Added documentation badge ro README.md and documentation home page

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, "../../deepof/")
+sys.path.insert(0, "../../")
 import sphinx_rtd_theme
 
 # -- Project information -----------------------------------------------------
