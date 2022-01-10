da370c5b6f615cab50bd65c39adb24ff46790447
Lucas Miranda <lucasmiranda42@gmail.com>
2022-01-10 10:29:29 +0100

    Fixed bugs in external model training script. Updated documentation index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -54,13 +54,13 @@ exclude_patterns = []
 #
 html_theme = "sphinx_rtd_theme"
 
-# Change the icon displayed in the web browser tab for the docs
-html_favicon = 'deepof.ico'
-
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+
+# Change the icon displayed in the web browser tab for the docs
+html_favicon = 'deepof.ico'
 
 # Autodoc default configuration
 autodoc_default_options = {
