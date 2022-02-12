Fixed bug in GMVAE's most recent implementation, which was accidentally removing KL and MMD loss terms during training

deepof/model_utils.py
@@ -495,10 +495,9 @@ class VectorQuantizer(tf.keras.layers.Layer):
             soft_counts = tf.nn.softmax(similarity, axis=1)
             return soft_counts
 
-        else:
-            # Return index of the closest code
-            encoding_indices = tf.argmin(distances, axis=1)
-            return encoding_indices
+        # Return index of the closest code
+        encoding_indices = tf.argmin(distances, axis=1)
+        return encoding_indices
 
     def update_posterior_variances(self):  # pragma: no cover
         """

deepof/models.py
@@ -1037,7 +1037,7 @@ class GMVAE(tf.keras.models.Model):
 
         """
 
-        pass  # TODO: compute posterior on the full dataset and store here
+        # TODO: compute posterior on the full dataset and store here
 
     @tf.function
     def call(self, inputs):