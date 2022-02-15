Fixed bug in GMVAE's most recent implementation, which was accidentally removing KL and MMD loss terms during training

deepof/model_utils.py
@@ -297,11 +297,16 @@ class GaussianMixtureLatent(tf.keras.models.Model):
                 probs=tf.ones(self.n_components) / self.n_components
             ),
             components_distribution=tfd.MultivariateNormalDiag(
-                loc=tf.keras.initializers.he_uniform()(
-                    [self.n_components, self.latent_dim],
+                loc=tfp.mcmc.sample_halton_sequence(
+                    num_results=self.n_components,
+                    dim=self.latent_dim,
+                    dtype=tf.float32,
+                    randomized=False,
+                    seed=None,
+                    name="GMVAE_prior_initialization",
                 ),
                 scale_diag=tf.ones([self.n_components, self.latent_dim])
-                / self.n_components,
+                / (2 * self.n_components),
             ),
         )
         self.cluster_overlap_layer = ClusterOverlap(
@@ -832,7 +837,6 @@ class ClusterOverlap(Layer):
         config.update({"enc": self.enc})
         config.update({"k": self.k})
         config.update({"loss_weight": self.loss_weight})
-        config.update({"min_confidence": self.min_confidence})
         return config
 
     def call(self, inputs, **kwargs):  # pragma: no cover
@@ -860,7 +864,7 @@ class ClusterOverlap(Layer):
             dtype=tf.dtypes.float32,
         )
 
-        min_confidence = 0.25  # 2 * 1 / tf.shape(hard_groups)[1]
+        min_confidence = tf.cast(2 * 1 / tf.shape(categorical)[1], tf.float32)
         n_components = tf.cast(
             tf.shape(
                 tf.unique(

deepof/models.py
@@ -281,7 +281,7 @@ def get_vqvae(
     input_shape: tuple,
     latent_dim: int,
     n_components: int,
-    beta: float = 10.0,
+    beta: float = 1.0,
     conv_filters=64,
     dense_layers=1,
     dense_activation="relu",
@@ -389,7 +389,7 @@ class VQVAE(tf.keras.models.Model):
         input_shape: tuple,
         latent_dim: int = 32,
         n_components: int = 15,
-        beta: float = 10.0,
+        beta: float = 1.0,
         architecture_hparams: dict = None,
         **kwargs,
     ):
@@ -504,10 +504,10 @@ class VQVAE(tf.keras.models.Model):
 
         with tf.GradientTape() as tape:
             # Get outputs from the full model
-            outputs = self.vqvae(x, training=True)
+            reconstructions = self.vqvae(x, training=True)
 
             # Compute losses
-            reconstruction_loss = -tf.reduce_mean(reconstructions.log_prob(next(y))) / 4
+            reconstruction_loss = -tf.reduce_mean(reconstructions.log_prob(next(y)))
             total_loss = reconstruction_loss + sum(self.vqvae.losses)
 
         # Backpropagation
@@ -549,10 +549,10 @@ class VQVAE(tf.keras.models.Model):
         y = (labels for labels in y)
 
         # Get outputs from the full model
-        reconstructions = self.vqvae(x)
+        reconstructions = self.vqvae(x, training=False)
 
         # Compute losses
-        reconstruction_loss = -tf.reduce_mean(reconstructions.log_prob(next(y))) / 4
+        reconstruction_loss = -tf.reduce_mean(reconstructions.log_prob(next(y)))
         total_loss = reconstruction_loss + sum(self.vqvae.losses)
 
         # Compute populated clusters
@@ -1194,10 +1194,6 @@ class GMVAE(tf.keras.models.Model):
         }
 
         # Add optional metrics to final log dict
-        if "ELBO" in self.latent_loss:
-            log_dict["kl_weight"] = self.val_kl_loss_weight_tracker.result()
-        if "MMD" in self.latent_loss:
-            log_dict["mmd_weight"] = self.val_mmd_loss_weight_tracker.result()
         if self.overlap_loss:
             log_dict["overlap_loss"] = self.val_overlap_loss_tracker.result()
         if self.next_sequence_prediction: