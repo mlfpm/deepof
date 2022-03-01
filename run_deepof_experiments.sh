b0a65f826ee17ad6980b201e3aad88e707bfaf43
Lucas Miranda <lucasmiranda42@gmail.com>
2022-03-01 02:33:53 +0100

    Added disentanglement penalization to the latent space in both GMVAE and VQVAE models

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -10,4 +10,4 @@ module load tensorflow-probability/0.14.1
 snakemake --snakefile deepof_experiments.smk --forceall --dag | dot -Tpdf > deepof_experiments_DAG.pdf
 snakemake --snakefile deepof_experiments.smk --forceall --rulegraph | dot -Tpdf > deepof_experiments_RULEGRAPH.pdf
 
-pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 25 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:2" > deepof_experiments.out 2> deepof_experiments.err
+pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 100 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:2" > deepof_experiments.out 2> deepof_experiments.err
