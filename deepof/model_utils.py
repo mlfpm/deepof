Fixed bug in SIWAE computation when model had multiple outputs. Added a ReduceOnPlateau callback to decrease learning rate on autoencoders when training stagnates

deepof/model_utils.py
@@ -400,6 +400,8 @@ class GaussianMixtureLatent(tf.keras.models.Model):
                     loc=gauss[1][..., : self.latent_dim],
                     scale_diag=1e-3 + gauss[1][..., self.latent_dim :],
                 ),
+                validate_args=True,
+                allow_nan_stats=False,
             ),
             convert_to_tensor_fn="sample",
             name="encoding_distribution",
@@ -418,6 +420,8 @@ class GaussianMixtureLatent(tf.keras.models.Model):
                 scale_diag=tf.ones([self.n_components, self.latent_dim])
                 / tf.math.sqrt(tf.cast(self.n_components, dtype=tf.float32) / 2.0),
             ),
+            validate_args=True,
+            allow_nan_stats=False,
         )
         self.cluster_control_layer = ClusterControl(
             batch_size=self.batch_size,
@@ -570,7 +574,7 @@ class VectorQuantizer(tf.keras.models.Model):
         # Add a disentangling penalty to the embeddings
         if self.reg_gram:
             gram_loss = compute_gram_loss(
-                x, weight=self.reg_gram, batch_size=input_shape
+                x, weight=self.reg_gram, batch_size=input_shape[0]
             )
             self.add_loss(gram_loss)
             self.add_metric(gram_loss, name="gram_loss")

deepof/train_utils.py
@@ -131,7 +131,7 @@ class ExponentialLearningRate(tf.keras.callbacks.Callback):
 
 
 def find_learning_rate(
-    model, X, y, epochs=1, batch_size=32, min_rate=10 ** -5, max_rate=10
+    model, data, epochs=1, batch_size=32, min_rate=10 ** -8, max_rate=10 ** -1
 ):
     """
 
@@ -139,8 +139,7 @@ def find_learning_rate(
 
     Args:
         model (tf.keras.Model): model to train
-        X (tf.Tensor): tensor containing the input data
-        y (tf.Tensor): tensor containing the target data
+        data (tuple): training data
         epochs (int): number of epochs to train the model for
         batch_size (int): batch size to use for training
         min_rate (float): minimum learning rate to consider
@@ -152,12 +151,12 @@ def find_learning_rate(
     """
 
     init_weights = model.get_weights()
-    iterations = len(X) // batch_size * epochs
+    iterations = len(data)
     factor = K.exp(K.log(max_rate / min_rate) / iterations)
     init_lr = K.get_value(model.optimizer.lr)
     K.set_value(model.optimizer.lr, min_rate)
     exp_lr = ExponentialLearningRate(factor)
-    model.fit(X, y, epochs=epochs, batch_size=batch_size, callbacks=[exp_lr])
+    model.fit(data, epochs=epochs, batch_size=batch_size, callbacks=[exp_lr])
     K.set_value(model.optimizer.lr, init_lr)
     model.set_weights(init_weights)
     return exp_lr.rates, exp_lr.losses

tests/test_train_utils.py
@@ -35,17 +35,17 @@ def test_load_treatments():
 def test_find_learning_rate():
     X = np.random.uniform(0, 10, [1500, 5])
     y = np.random.randint(0, 2, [1500, 1])
+    dataset = tf.data.Dataset.from_tensors((X, y))
 
     test_model = tf.keras.Sequential()
-    test_model.add(tf.keras.layers.Dense(1))
+    test_model.add(tf.keras.layers.Dense(1, input_shape=X.shape[1:]))
 
     test_model.compile(
         loss=tf.keras.losses.binary_crossentropy,
         optimizer=tf.keras.optimizers.SGD(),
     )
-    test_model.build(X.shape)
 
-    deepof.train_utils.find_learning_rate(test_model, X, y)
+    deepof.train_utils.find_learning_rate(test_model, data=dataset)
 
 
 @given(