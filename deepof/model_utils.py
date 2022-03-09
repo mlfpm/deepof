Fixed bug in SIWAE computation when model had multiple outputs. Added a ReduceOnPlateau callback to decrease learning rate on autoencoders when training stagnates

deepof/model_utils.py
@@ -258,6 +258,10 @@ def compute_siwae(prior, likelihood, posterior, x, T):  # pragma: no cover
     q = posterior(x)
     z = q.components_distribution.sample(T)
     z = tf.transpose(z, perm=[1, 0, 2, 3])
+    lk = likelihood(x)
+    if isinstance(lk, list):
+        lk = lk[0]
+
     loss_n = (
         tf.math.reduce_logsumexp(
             (
@@ -268,7 +272,7 @@ def compute_siwae(prior, likelihood, posterior, x, T):  # pragma: no cover
                     tf.expand_dims(
                         tf.expand_dims(
                             tf.reduce_sum(
-                                likelihood(x).log_prob(x),
+                                lk.log_prob(x),
                                 axis=-1,
                             ),
                             axis=-1,

deepof/train_utils.py
@@ -260,7 +260,11 @@ def get_callbacks(
         profile_batch=2,
     )
 
-    callbacks = [run_ID, tensorboard_callback]
+    reduce_lr_callback = tf.keras.callbacks.ReduceLROnPlateau(
+        monitor="val_loss", factor=0.5, patience=5, min_lr=1e-8
+    )
+
+    callbacks = [run_ID, tensorboard_callback, reduce_lr_callback]
 
     if cp:
         cp_callback = tf.keras.callbacks.ModelCheckpoint(