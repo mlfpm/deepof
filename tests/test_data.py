a910ddcb408ef86734531985d09533812f84a4ff
lucasmir <lucas_miranda@psych.mpg.de>
2022-07-04 12:58:48 +0200

    Shortened multi-animal test data

--- tests/test_data.py
+++ tests/test_data.py
@@ -173,7 +173,7 @@ def test_get_supervised_annotation():
     assert prun._type == "supervised"
 
 
-@settings(max_examples=25, deadline=None, derandomize=True)
+@settings(max_examples=36, deadline=None, derandomize=True)
 @given(
     nodes=st.integers(min_value=0, max_value=1),
     mode=st.one_of(st.just("single"), st.just("multi"), st.just("madlc")),
