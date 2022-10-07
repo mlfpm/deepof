9edb3775f740f0fee443688a0266efdaf3e80810
lucas_miranda <lucas_miranda@psych.mpg.de>
2022-10-07 12:40:59 +0200

    Updated docstrings documentation

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,15 +46,10 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
-# Define mock imports for RTD
-with open("../../requirements.txt") as reqs:
-    autodoc_mock_imports = [re.findall("(.+?)=", i)[0] for i in reqs]
-
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
