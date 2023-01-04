a2e5614b48ea7b02a6f8b4e49328f7119d14d9b3
lucas_miranda <lucas_miranda@psych.mpg.de>
2023-01-04 12:41:29 +0100

    Updated cluster scripts to handle multi animal embeddings and graph data

--- run_deepof_experiments.sh
+++ run_deepof_experiments.sh
@@ -14,4 +14,4 @@ source activate deepof
 snakemake --snakefile deepof_experiments.smk --forceall --dag | dot -Tpdf > deepof_experiments_DAG.pdf
 snakemake --snakefile deepof_experiments.smk --forceall --rulegraph | dot -Tpdf > deepof_experiments_RULEGRAPH.pdf
 
-pipenv run snakemake --snakefile deepof_experiments.smk deepof_experiments -j 100 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
+snakemake --snakefile deepof_experiments.smk deepof_experiments -j 100 --latency-wait 15 --cluster-config cluster.json --cluster "sbatch --time={cluster.time} --mem={cluster.mem} -o {cluster.stdout} -e {cluster.stderr} --job-name={cluster.jobname} --cpus-per-task={cluster.cpus} --constraint="gpu" --gres=gpu:gtx980:1" > deepof_experiments.out 2> deepof_experiments.err
