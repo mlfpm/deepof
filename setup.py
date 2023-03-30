5f15ec7edef992d30430383f44f3a81f51c2a382
lucas_miranda <lucas_miranda@psych.mpg.de>
2023-03-31 00:51:05 +0200

    Added colab links to tutorials

--- setup.py
+++ setup.py
@@ -6,14 +6,6 @@ with open("README.md", "r") as fh:
 with open("requirements.txt", "r") as fp:
     install_requires = [pkg.replace("\n", "") for pkg in fp]
 
-# Add system specific dependencies
-install_requires += [
-    "tensorflow>=2.2.0;platform_machine != 'arm64' or platform_system != 'Darwin'",
-    "tensorflow-macos>=2.5.0;platform_machine == 'arm64' and platform_system == 'Darwin'",
-    "keras-tcn==3.5.0;platform_machine != 'arm64' or platform_system != 'Darwin'",
-    "keras-tcn-macos==1.0;platform_machine == 'arm64' and platform_system == 'Darwin'",
-]
-
 setuptools.setup(
     name="deepof",
     version="0.2",
