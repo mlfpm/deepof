be923df24b0770383b748fd14ab7658f23791afd
lucas_miranda <lucas_miranda@psych.mpg.de>
2023-05-05 01:02:14 +0200

    Fixed paths in unsupervised tutorial, refactored tests

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -56,6 +56,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "sphinx_rtd_theme"
+def setup(app):
+    app.add_css_file('custom.css')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
