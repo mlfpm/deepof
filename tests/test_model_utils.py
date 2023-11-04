f718c780860759a489d1691e647c88d788bea5e5
lucasmiranda <miranda@biochem.mpg.de>
2023-11-04 09:42:56 +0100

    Updated requirements.txt for new documentation

--- tests/test_model_utils.py
+++ tests/test_model_utils.py
@@ -75,7 +75,7 @@ def test_get_hard_counts(soft_counts):
     assert isinstance(hard_counts, tf.Tensor)
 
 
-@settings(max_examples=1, deadline=None, suppress_health_check=[HealthCheck.too_slow])
+@settings(max_examples=18, deadline=None, suppress_health_check=[HealthCheck.too_slow])
 @given(
     embedding_model=st.sampled_from(["VQVAE", "VaDE", "Contrastive"]),
     encoder_type=st.sampled_from(["recurrent", "TCN", "transformer"]),
