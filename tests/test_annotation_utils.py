ba52b30393223d7712b40d0c3d1ff71045dd2eab
Patrick TS <patrickschlegel93@outlook.de>
2025-03-10 16:14:00 +0100

    Forgot to update test

--- tests/test_annotation_utils.py
+++ tests/test_annotation_utils.py
@@ -365,7 +365,7 @@ def test_rule_based_tagging(multi_animal, video_output):
     rmtree(os.path.join(path, "deepof_project"))
 
     assert isinstance(hardcoded_tags, deepof.data.TableDict)
-    assert list(hardcoded_tags.values())[0].shape[1] == (33 if multi_animal else 12)
+    assert list(hardcoded_tags.values())[0].shape[1] == (29 if multi_animal else 10)
 
 
 # list of valid polygons as ill-defined polygons (e.g. lines) can lead to deviations
